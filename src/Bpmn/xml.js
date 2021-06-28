0aca587937d7d6aada2f8a1e80a48ee02fe086e5
lihaitao <18268187601@163.com>
2021-06-28 16:29:08 +0800

    将isExecutable字段改为true

--- src/Bpmn/xml.js
+++ src/Bpmn/xml.js
@@ -1,6 +1,6 @@
 export const bpmnXML = `<?xml version="1.0" encoding="UTF-8"?>
     <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:omgdi="http://www.omg.org/spec/DD/20100524/DI" xmlns:omgdc="http://www.omg.org/spec/DD/20100524/DC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" id="sid-38422fae-e03e-43a3-bef4-bd33b32041b2" targetNamespace="http://bpmn.io/bpmn" exporter="bpmn-js (https://demo.bpmn.io)" exporterVersion="5.1.2">
-        <process id="Process_1" isExecutable="false">
+        <process id="Process_1" isExecutable="true">
             <startEvent id="StartEvent_1y45yut" name="开始">
             <outgoing>SequenceFlow_0h21x7r</outgoing>
             </startEvent>
